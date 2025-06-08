a043fbff1c68aacc8c796b3f784444c426622c6a
cxcarvaj <cxcarvaj@gmail.com>
2025-06-08 13:44:36 -0500

    Initial Commit

---
+++ AIApp/AIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIAppApp.swift
+//  AIApp
+//
+//  Created by Carlos Xavier Carvajal Villegas on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
